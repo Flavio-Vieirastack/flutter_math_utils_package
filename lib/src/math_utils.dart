b24f6e831e61ac34bfb25868a0b5ab51f55b104d
Flavio Vieira <flavioemerson1@outlook.com>
2022-03-12 19:19:17 -0300

    criado testes para multiplicação e soma e metodo de multiplicação

--- lib/src/math_utils.dart
+++ lib/src/math_utils.dart
@@ -1,12 +1,25 @@
-
 class MathUtils {
   MathUtils._();
 
   static num sum(List<num> numbers) {
-    num sum = 0;
-    for (num number in numbers) {
-      sum += number;
+    if (numbers.isNotEmpty) {
+      num sum = 0;
+      for (num number in numbers) {
+        sum += number;
+      }
+      return sum;
     }
-    return sum;
+    throw Exception();
+  }
+
+  static num multiply (List<num> numbers) {
+    if (numbers.isNotEmpty) {
+      num product = 1;
+      for (num number in numbers) {
+        product *= number;
+      }
+      return product;
+    }
+    throw Exception();
   }
 }
--- test/src/sum_test.dart
+++ test/src/sum_test.dart
@@ -2,8 +2,34 @@ import 'package:flutter_math_utils/flutter_math_utils.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Should return the correct number of sum', () {
-    final sut = MathUtils.sum([2, 2]);
-    expect(sut, 4);
+  group('sum tests', () {
+    test('Should return the correct number of sum', () {
+      final sut = MathUtils.sum([2, 2]);
+      expect(sut, 4);
+    });
+    test('Should return the correct number of sum with doubles', () {
+      final sut = MathUtils.sum([2.5, 2.5]);
+      expect(sut, 5);
+    });
+    test('Should thow an exception', () {
+      const sut = MathUtils.sum;
+      expect(() => sut([]), throwsException);
+    });
   });
+
+  group('multiply tests', () {
+    test('Should return the correct number of multiply', () {
+      final sut = MathUtils.multiply([2, 2]);
+      expect(sut, 4);
+    });
+    test('Should return the correct number of multiply with doubles', () {
+      final sut = MathUtils.multiply([2.5, 2.5]);
+      expect(sut, 6.25);
+    });
+    test('Should thow an exception', () {
+      const sut = MathUtils.multiply;
+      expect(() => sut([]), throwsException);
+    });
+  });
+  
 }
